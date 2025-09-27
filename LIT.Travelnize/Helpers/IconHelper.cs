ad100c02e3a311aba1cc0fad30d1334bc00adeec
Matthias Lennips <matthiaslennips@hotmail.com>
2025-09-27 15:07:25 +0200

    Icons aktualisiert und Benutzeroberfläche verbessert
    In dieser Änderung wurde die `IconHelper`-Klasse hinzugefügt, um statische Eigenschaften für verschiedene Reise-Icons zu definieren.
    
    Die Datei `Home.razor` wurde angepasst, um die neuen Icons zu verwenden und neue Abschnitte für Unterkunft und Aktivitäten hinzuzufügen.
    
    In `Trip.razor` wurden die Icons aktualisiert und die Tab-Struktur verbessert.
    
    Zusätzlich wurden in `Trips.razor` Layout- und Stiländerungen vorgenommen, um die Lesbarkeit zu erhöhen und die Benutzererfahrung zu optimieren.

--- LIT.Travelnize/Helpers/IconHelper.cs
+++ LIT.Travelnize/Helpers/IconHelper.cs
@@ -1,6 +1,37 @@
-﻿namespace LIT.Travelnize.Helpers
+﻿
+
+namespace LIT.Travelnize.Helpers
 {
     public class IconHelper
     {
+        public static string TripFilled => MudBlazor.Icons.Material.Filled.TravelExplore;
+        public static string TripOutlined => MudBlazor.Icons.Material.Outlined.TravelExplore;
+        public static string TripRounded => MudBlazor.Icons.Material.Rounded.TravelExplore;
+        public static string TripSharp => MudBlazor.Icons.Material.Sharp.TravelExplore;
+        public static string TripTwoTone => MudBlazor.Icons.Material.TwoTone.TravelExplore;
+
+        public static string TravelSegmentFilled => MudBlazor.Icons.Material.Filled.Route;
+        public static string TravelSegmentOutlined => MudBlazor.Icons.Material.Outlined.Route;
+        public static string TravelSegmentRounded => MudBlazor.Icons.Material.Rounded.Route;
+        public static string TravelSegmentSharp => MudBlazor.Icons.Material.Sharp.Route;
+        public static string TravelSegmentTwoTone => MudBlazor.Icons.Material.TwoTone.Route;
+
+        public static string DestinationFilled => MudBlazor.Icons.Material.Filled.LocationOn;
+        public static string DestinationOutlined => MudBlazor.Icons.Material.Outlined.LocationOn;
+        public static string DestinationRounded => MudBlazor.Icons.Material.Rounded.LocationOn;
+        public static string DestinationSharp => MudBlazor.Icons.Material.Sharp.LocationOn;
+        public static string DestinationTwoTone => MudBlazor.Icons.Material.TwoTone.LocationOn;
+
+        public static string AccommodationFilled => MudBlazor.Icons.Material.Filled.Hotel;
+        public static string AccommodationOutlined => MudBlazor.Icons.Material.Outlined.Hotel;
+        public static string AccommodationRounded => MudBlazor.Icons.Material.Rounded.Hotel;
+        public static string AccommodationSharp => MudBlazor.Icons.Material.Sharp.Hotel;
+        public static string AccommodationTwoTone => MudBlazor.Icons.Material.TwoTone.Hotel;
+
+        public static string ActivityFilled => MudBlazor.Icons.Material.Filled.Attractions;
+        public static string ActivityOutlined => MudBlazor.Icons.Material.Outlined.Attractions;            
+        public static string ActivityRounded => MudBlazor.Icons.Material.Rounded.Attractions;
+        public static string ActivitySharp => MudBlazor.Icons.Material.Sharp.Attractions;
+        public static string ActivityTwoTone => MudBlazor.Icons.Material.TwoTone.Attractions;
     }
 }
